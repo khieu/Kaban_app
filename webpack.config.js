29fd367d75ec8b48d4b0300dda64363127fd3f2e
khieu <hieule246@gmail.com>
2016-02-12 02:10:22 -0500

    Implement a basic note application

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,19 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
 
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -30,7 +38,19 @@ const common = {
          loaders: ['style', 'css'],
          // Include accepts either a path or an array of paths.
          include: PATHS.app
-       }
+       },
+
+       // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app
+      }
      ]
    }
 };
