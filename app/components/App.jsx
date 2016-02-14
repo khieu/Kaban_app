a0c4cc33bd0d76f90195cd9353bae6bacccc4d83
khieu <hieule246@gmail.com>
2016-02-14 00:27:00 -0500

    React & Flux: creating stores and actions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,9 @@
-import uuid from 'node-uuid';
+//import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Note';
 import Notes from './Notes';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 /*
 var App = React.createClass({
@@ -39,23 +41,21 @@ export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			notes: [
-				{
-					id: uuid.v4(),
-					task: 'Learn Webpack'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Learn React'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Do laundry'
-				}
-			]
-		};
+		this.state = NoteStore.getState();
+		console.log(this.state);
 	}
+
+	ComponentDidMount() {
+		NoteStore.listen(this.storeChanged);
+	}
+
+	ComponentWillUnmount() {
+		NoteStore.unlisten(this.storeChanged);
+	}
+
+	storeChanged = (state) => {
+		this.setState(state);
+	};
 
 	render() {
 		const notes = this.state.notes;
@@ -73,32 +73,15 @@ export default class App extends React.Component {
 	}
 
 	addNote = () => {
-		this.setState({
-			notes: this.state.notes.concat([{
-				id: uuid.v4(),
-				task: 'New task'
-			}])
-		});
+		console.log('addNote called');
+		NoteActions.create({task: 'New task'});
 	};
 
 	editNote = (id, task) => {
-		const notes = this.state.notes.map(
-			note => {
-				if (note.id === id && task) {
-					note.task = task
-				}
-
-				return note;
-			}
-		);
-
-		this.setState({notes});
+		NoteActions.update({id, task});
 	};
 
 	deleteNote = (id) => {
-
-		this.setState({
-			notes: this.state.notes.filter(note => note.id !== id)
-		});
+		NoteActions.delete(id);
 	};
 }
---
+++ app/stores/NoteStore.js
@@ -0,0 +1,58 @@
+import uuid from 'node-uuid';
+import alt from '../libs/alt';
+import NoteActions from '../actions/NoteActions';
+
+class NoteStore {
+	constructor() {
+		this.bindActions(NoteActions);
+
+		this.notes = [{
+				id: uuid.v4(),
+				task: 'Learn Webpack'
+			},
+			{
+				id: uuid.v4(),
+				task: 'Learn React'
+			},
+			{
+				id: uuid.v4(),
+				task: 'Do laundry'
+			}];
+	}
+
+	create(note) {
+		const notes = this.notes;
+
+		note.id = uuid.v4();
+		console.log(note);
+
+		this.setState({
+			notes: notes.concat(note)
+		});
+	}
+
+	update(updatedNote) {
+		const notes = this.notes.map(
+			note => {
+				if (note.id === updatedNote.id) {
+					//note.task = updatedNote.task;
+					// OR
+					return Object.assign({}, note, updatedNote);
+				}
+				return note;
+			}
+		);
+
+		//this.setState({notes: notes});
+		// OR
+		this.setState({notes});
+	}
+
+	delete(id) {
+		this.setState({
+			notes: this.notes.filter(note => note.id !== id)
+		});
+	}
+}
+
+export default alt.createStore(NoteStore, 'NoteStore');
